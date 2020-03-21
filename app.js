221042739c2947944f124ce77e6affc006c16fce
desirekaleba <desirekaleba@gmail.com>
2020-03-21 23:46:44 +0300

    app.js and index.html init

---
+++ app.js
@@ -0,0 +1,22 @@
+// let total = 0, count = 1;
+// while (count <= 10) {
+//     total += count;
+//     count += 1;
+// }
+// console.log(total);
+
+// Factorial example
+/*function factorial(n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return factorial(n - 1) * n;
+    }
+}
+let heightFactorial = factorial(8);
+console.log(heightFactorial);*/
+
+// String comparison
+/*console.log('Zb' < 'za'); // true
+*/
+
