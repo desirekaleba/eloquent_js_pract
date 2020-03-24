27a44ab99a5335428f5fc1a5a658f4e66bbca010
desirekaleba <desirekaleba@gmail.com>
2020-03-25 00:40:12 +0300

    Matrix class

--- app.js
+++ app.js
@@ -399,7 +399,29 @@ console.log(`Boris is ${ages.get("Boris")}`);
 console.log("Is Deska's age known ?", ages.has("Deska"));*/
 
 // Symbol iterators
-let okIterator = "OK"[Symbol.iterator]();
+/*let okIterator = "OK"[Symbol.iterator]();
 console.log(okIterator.next());
 console.log(okIterator.next());
-console.log(okIterator.next());
+console.log(okIterator.next());*/
+
+// Matrix
+class Matrix {
+    constructor(width, height, element = (x, y) => undefined) {
+        this.width = width;
+        this.height = height;
+        this.content = [];
+
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++){
+                this.content[y * width + x] = element(x, y);
+            }
+        }
+    }
+
+    get(x, y) {
+        return this.content[y * this.width + x];
+    }
+    set(x, y, value) {
+        this.content[y * this.width + x] = value;
+    }
+}
