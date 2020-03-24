45e3c4055bb4c7b9f11ce7c1a4a4f2ee2d7b78ab
desirekaleba <desirekaleba@gmail.com>
2020-03-25 00:58:30 +0300

    Symbol.iterator logic implemented using Matrix and MatrixIterator

--- app.js
+++ app.js
@@ -460,3 +460,8 @@ class MatrixIterator {
         }
     }
 }
+// looping through 
+let matrix = new Matrix(2, 2, (x, y) => ` value ${x}, ${y}`);
+for (let {x, y, value} of matrix) {
+    console.log(x, y, value);
+}
