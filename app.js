61e9c33cc5f92a3ffd2aacd27ed9e594f3fd93a8
desirekaleba <desirekaleba@gmail.com>
2020-03-27 12:36:43 +0300

    regex, creed, stripComments [remove comments from a code]

--- app.js
+++ app.js
@@ -819,7 +819,7 @@ function stripComments(code) {
     return code.replace(/\/\/.*|\/\*[^]*\*\//g, "");
 }
 console.log(stripComments("1 + /* 2 */3"));
-console.log(stripComments("x = 10;// ten!"));
+console.log(stripComments("x = 10;// ten!!"));
 // → x = 10;
 console.log(stripComments("1 /* a */+/* b */ 1"));
 // → 1 1
