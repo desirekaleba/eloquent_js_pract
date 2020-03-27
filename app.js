d61a2a311d60db77e29d4cf84bafc33136b87c43
desirekaleba <desirekaleba@gmail.com>
2020-03-27 14:26:24 +0300

    regex, parseINI func, parse an INI file

--- app.js
+++ app.js
@@ -845,9 +845,48 @@ console.log(pattern.test("1524acdfh"));*/
 // // → ["an", "an"]
 
 // Looping over matches
-let input = "A string with 3 numbers in it... 42 and 88.";
-let number = /\b\d+\b/g;
-let match;
-while (match = number.exec(input)) {
-    console.log(match + " Found" + " at " + match.index);
+// let input = "A string with 3 numbers in it... 42 and 88.";
+// let number = /\b\d+\b/g;
+// let match;
+// while (match = number.exec(input)) {
+//     console.log(match + " Found" + " at " + match.index);
+// }
+
+// Parsing the INI file
+function parseINI(str) {
+    let result = {};
+    let section = result;
+
+    str.split(/\r?\n/)
+        .forEach(line => {
+            if (match = line.match(/^(\w+)=(.*)$/)) {
+                section[match[1]] = match[2];
+            } else if (match = line.match(/^\[(.*)\]$/)) {
+                section = result[match[1]] = {};
+            } else if (!/^\s*(;.*)?$/.test(line)) {
+                throw new Error("Line '" + line + "' is not valid");
+            }
+        });
+    return result;
 }
+
+console.log(parseINI(`
+name=Vasilis
+[address]
+city=Tessaloniki`));
+
+let criminals = `
+searchengine=https://duckduckgo.com/?q=$1
+spitefulness=9.7
+; comments are preceded by a semicolon...
+; each section concerns an individual enemy
+[larry]
+fullname=Larry Doe
+type=kindergarten bully
+website=http://www.geocities.com/CapeCanaveral/11451
+[davaeorn]
+fullname=Davaeorn
+type=evil wizard
+outputdir=/home/marijn/enemies/davaeorn`;
+
+console.log(parseINI(criminals));
