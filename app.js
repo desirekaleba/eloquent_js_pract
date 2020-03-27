a59a2c4d96456cfe1a523c295b0cd0b229cb71f8
desirekaleba <desirekaleba@gmail.com>
2020-03-27 15:55:20 +0300

    regex, exercise (regexp golf)

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ console.log(heightFactorial);*/
 
 // String comparison
 /*console.log('Zb' < 'za'); // true
-*/
+ */
 
 // use of isNaN
 /*let number = Number(prompt("Enter a number"));
@@ -717,7 +717,7 @@ console.log(box.locked); */
 
 // Matching more than one digit character
 /* let _digits = /\d+/;
-// let _digts = /\d*///;
+// let _digts = /\d*/ //;
 // console.log(_digits.test("1234"));
 
 // let neighbor = /neighbou?r/;
@@ -893,17 +893,60 @@ outputdir=/home/marijn/enemies/davaeorn`;
 console.log(parseINI(criminals));*/
 
 // internal characters
-console.log(/🍎{3}/.test("🍎🍎🍎"));
-// → false
-console.log(/<.>/.test("<🌹>"));
-// → false
-console.log(/<.>/u.test("<🌹>"));
-// → true
-console.log(/\p{Script=Greek}/u.test("α"));
-// → true
-console.log(/\p{Script=Arabic}/u.test("α"));
-// → false
-console.log(/\p{Alphabetic}/u.test("α"));
-// → true
-console.log(/\p{Alphabetic}/u.test("!"));
-// → false
+// console.log(/🍎{3}/.test("🍎🍎🍎"));
+// // → false
+// console.log(/<.>/.test("<🌹>"));
+// // → false
+// console.log(/<.>/u.test("<🌹>"));
+// // → true
+// console.log(/\p{Script=Greek}/u.test("α"));
+// // → true
+// console.log(/\p{Script=Arabic}/u.test("α"));
+// // → false
+// console.log(/\p{Alphabetic}/u.test("α"));
+// // → true
+// console.log(/\p{Alphabetic}/u.test("!"));
+// // → false
+
+// Regexp golf
+verify(/ca[rt]/,
+    ["my car", "bad cats"],
+    ["camper", "high art"]);
+
+verify(/pr?op/,
+    ["pop culture", "mad props"],
+    ["plop", "prrrop"]);
+
+verify(/ferr(et|y|ari)/,
+    ["ferret", "ferry", "ferrari"],
+    ["ferrum", "transfer A"]);
+
+verify(/ious\b/,
+    ["how delicious", "spacious room"],
+    ["ruinous", "consciousness"]);
+
+verify(/\s[.,:;]/,
+    ["bad punctuation ."],
+    ["escape the dot"]);
+
+verify(/\w{7}/,
+    ["hottentottententen"],
+    ["no", "hotten totten tenten"]);
+
+verify(/\b[^\We]+\b/i,
+    ["red platypus", "wobbling nest"],
+    ["earth bed", "learning ape", "BEET"]);
+
+
+function verify(regexp, yes, no) {
+    // Ignore unfinished exercises
+    if (regexp.source == "...") return;
+    for (let str of yes)
+        if (!regexp.test(str)) {
+            console.log(`Failure to match '${str}'`);
+        }
+    for (let str of no)
+        if (regexp.test(str)) {
+            console.log(`Unexpected match for '${str}'`);
+        }
+}
