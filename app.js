a2fc6d8f392681b5310717c88118be8481e72610
desirekaleba <desirekaleba@gmail.com>
2020-03-27 16:07:27 +0300

    regex, exercise (numbers again)

--- app.js
+++ app.js
@@ -952,5 +952,24 @@ console.log(parseINI(criminals));*/
 // }
 
 // Quoting style
-let text = "'I'm the cook,' he said, 'it's my job.'";
-console.log(text.replace(/(^|\W)'|'(\W|$)/g, ""));
+// let text = "'I'm the cook,' he said, 'it's my job.'";
+// console.log(text.replace(/(^|\W)'|'(\W|$)/g, ""));
+
+// Numbers again
+let number = /^[+\-]?(\d+(\.\d*)?|\.\d+)([eE][+\-]?\d+)?$/;
+
+// Tests:
+for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+        "1.3e2", "1E-4", "1e+12"
+    ]) {
+    if (!number.test(str)) {
+        console.log(`Failed to match '${str}'`);
+    }
+}
+for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+        ".5.", "1f5", "."
+    ]) {
+    if (number.test(str)) {
+        console.log(`Incorrectly accepted '${str}'`);
+    }
+}
