7555e731a40dc6fc421dfcf3d2235382e168b478
desirekaleba <desirekaleba@gmail.com>
2020-03-28 13:44:16 +0300

    modules, date-format module created

--- format-date.js
+++ format-date.js
@@ -1,7 +1,7 @@
 const ordinal = require("ordinal");
 const {days, months} = require("date-names");
 
-exports.formatDate = function(date, format) {
+module.exports.formatDate = function(date, format) {
     return format.replace(/YYYY|M(MMM)?|Do?|dddd/g, tag => {
         if (tag == "YYYY")
             return date.getFullYear();
