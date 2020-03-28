3cee2ad9b1d6b10baa710086a7a424778d3a7808
desirekaleba <desirekaleba@gmail.com>
2020-03-29 00:41:46 +0300

    async functions

--- app.js
+++ app.js
@@ -1041,99 +1041,116 @@ console.log(weekDay.name(weekDay.number("Sunday")));*/
 //     })
 //     .then(value => console.log("Handler 2", value));
 
-class Timeout extends Error {}
+// class Timeout extends Error {}
 
-function request(nest, target, type, content) {
-    return new Promise((resolve, reject) => {
-        let done = false;
+// function request(nest, target, type, content) {
+//     return new Promise((resolve, reject) => {
+//         let done = false;
 
-        function attempt(n) {
-            nest.send(target, type, content, (failed, value) => {
-                done = true;
-                if (failed) reject(failed);
-                else resolve(value);
-            });
-            setTimeout(() => {
-                if (done) return;
-                else if (n < 3) attempt(n + 1);
-                else reject(new Timeout("Timed out"));
-            }, 250);
-        }
-        attempt(1);
-    });
-}
+//         function attempt(n) {
+//             nest.send(target, type, content, (failed, value) => {
+//                 done = true;
+//                 if (failed) reject(failed);
+//                 else resolve(value);
+//             });
+//             setTimeout(() => {
+//                 if (done) return;
+//                 else if (n < 3) attempt(n + 1);
+//                 else reject(new Timeout("Timed out"));
+//             }, 250);
+//         }
+//         attempt(1);
+//     });
+// }
 
-function requestType(name, handler) {
-    defineRequestType(name, (nest, content, source, callback) => {
+// function requestType(name, handler) {
+//     defineRequestType(name, (nest, content, source, callback) => {
+//         try {
+//             Promise.resolve(handler(nest, content, source))
+//                 .then(response => callback(null, response),
+//                     failure => callback(failure));
+//         } catch (exception) {
+//             callback(exception);
+//         }
+//     });
+// }
+// // collections of promises
+// requestType("ping", () => "pong");
+
+// function availableNeighbors(nest) {
+//     let requests = nest.neighbors.map(neighbor => {
+//         return request(nest, neighbor, "ping")
+//             .then(() => true, () => false);
+//     });
+//     return Promise.all(requests).then(result => {
+//         return nest.neighbors.filter((_, i) => result[i]);
+//     });
+// }
+
+// // Flooding
+// import {
+//     everywhere
+// } from "./crow-tech";
+
+// everywhere(nest => {
+//     nest.state.gossip = [];
+// });
+
+// function sendGossip(nest, message, exceptFor = null) {
+//     nest.state.gossip.push(message);
+//     for (let neighbor of nest.neighbors) {
+//         if (neighbor == exceptFor) continue;
+//         request(nest, neighbor, "gossip", message);
+//     }
+// }
+
+// requestType("gossip", (nest, message, source) => {
+//     if (nest.state.gossip.includes(message)) return;
+//     console.log(`${nest.name} received gossip '${message}' from ${source}`);
+//     sendGossip(nest, message, source);
+// });
+
+// // Message routing
+// requestType("connections", (nest, {
+//         name,
+//         neighbors
+//     },
+//     source) => {
+//     let connections = nest.state.connections;
+//     if (JSON.stringify(connections.get(name)) ==
+//         JSON.stringify(neighbors)) return;
+//     connections.set(name, neighbors);
+//     broadcastConnections(nest, name, source);
+// });
+
+// function broadcastConnections(nest, name, exceptFor = null) {
+//     for (let neighbor of nest.neighbors) {
+//         if (neighbor == exceptFor) continue;
+//         request(nest, neighbor, "connections", {
+//             name,
+//             neighbors: nest.state.connections.get(name)
+//         });
+//     }
+// }
+// everywhere(nest => {
+//     nest.state.connections = new Map;
+//     nest.state.connections.set(nest.name, nest.neighbors);
+//     broadcastConnections(nest, nest.name);
+// });
+
+// ASYNC functions
+async function findInStorage(nest, name) {
+    let local = await storage(nest, name);
+    if (local != null) return local;
+    let sources = network(nest).filter(n => n != nest.name);
+    while (sources.length > 0) {
+        let source = sources[Math.floor(Math.random() * sources.length)];
+        sources = sources.filter(n => n != source);
         try {
-            Promise.resolve(handler(nest, content, source))
-                .then(response => callback(null, response),
-                    failure => callback(failure));
-        } catch (exception) {
-            callback(exception);
-        }
-    });
-}
-// collections of promises
-requestType("ping", () => "pong");
-
-function availableNeighbors(nest) {
-    let requests = nest.neighbors.map(neighbor => {
-        return request(nest, neighbor, "ping")
-            .then(() => true, () => false);
-    });
-    return Promise.all(requests).then(result => {
-        return nest.neighbors.filter((_, i) => result[i]);
-    });
-}
-
-// Flooding
-import {
-    everywhere
-} from "./crow-tech";
-
-everywhere(nest => {
-    nest.state.gossip = [];
-});
-
-function sendGossip(nest, message, exceptFor = null) {
-    nest.state.gossip.push(message);
-    for (let neighbor of nest.neighbors) {
-        if (neighbor == exceptFor) continue;
-        request(nest, neighbor, "gossip", message);
+            let found = await routeRequest(nest, source, "storage",
+                name);
+            if (found != null) return found;
+        } catch (_) {}
     }
+    throw new Error("Not found");
 }
-
-requestType("gossip", (nest, message, source) => {
-    if (nest.state.gossip.includes(message)) return;
-    console.log(`${nest.name} received gossip '${message}' from ${source}`);
-    sendGossip(nest, message, source);
-});
-
-// Message routing
-requestType("connections", (nest, {
-        name,
-        neighbors
-    },
-    source) => {
-    let connections = nest.state.connections;
-    if (JSON.stringify(connections.get(name)) ==
-        JSON.stringify(neighbors)) return;
-    connections.set(name, neighbors);
-    broadcastConnections(nest, name, source);
-});
-
-function broadcastConnections(nest, name, exceptFor = null) {
-    for (let neighbor of nest.neighbors) {
-        if (neighbor == exceptFor) continue;
-        request(nest, neighbor, "connections", {
-            name,
-            neighbors: nest.state.connections.get(name)
-        });
-    }
-}
-everywhere(nest => {
-    nest.state.connections = new Map;
-    nest.state.connections.set(nest.name, nest.neighbors);
-    broadcastConnections(nest, nest.name);
-});
