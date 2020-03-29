a1c010982bff34c81c55260c1774015e9caec25a
desirekaleba <desirekaleba@gmail.com>
2020-03-29 23:37:12 +0300

    special forms, if

--- projects/programming_language/app.js
+++ projects/programming_language/app.js
@@ -92,3 +92,15 @@ function evaluate(expr, scope) {
         }
     }
 }
+
+// Special Forms
+// if
+specialForms.if = (args, scope) => {
+    if (args.length != 3) {
+        throw new SyntaxError("Wrong number of args to if");
+    } else if (evaluate(args[0], scope) !== false) {
+        return evaluate(args[1], scope);
+    } else {
+        return evaluate(args[2], scope);
+    }
+};
