7bab1f1d1e65bad36858e7f0ae7995f8033d1228
desirekaleba <desirekaleba@gmail.com>
2020-03-30 00:39:35 +0300

    an example of a plusOne function in Egg

--- projects/programming_language/app.js
+++ projects/programming_language/app.js
@@ -198,3 +198,8 @@ specialForms.fun = (args, scope) => {
         return evaluate(body, localScope);
     };
 };
+
+run(
+    `do(define(plusOne, fun(a, +(a, 1))),
+        print(plusOne(101)))`
+);
