0396252e622149e975da46dcae8ce114ffae8620
desirekaleba <desirekaleba@gmail.com>
2020-03-30 01:05:18 +0300

    the environment, top scope, array

--- projects/programming_language/app.js
+++ projects/programming_language/app.js
@@ -211,3 +211,22 @@ run(`
             *(base, pow(base, -(exp, 1)))))),
     print(pow(2, 10)))
 `);
+
+// * Compilation
+
+// * Exercises
+// arrays 
+topScope.array = (...values) => values;
+topScope.length = array => array.length;
+topScope.element = (array, i) => array[i];
+
+run(`
+    do(define(sum, fun(array,
+        do(define(i, 0),
+            define(sum, 0),
+            while(<(i, length(array)),
+                do(define(sum, +(sum, element(array, i))),
+                    define(i, +(i, 1)))),
+            sum))),
+        print(sum(array(1, 2, 3))))
+`);
