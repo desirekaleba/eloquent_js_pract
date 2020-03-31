9ab913ea90303d92731c4adc3600b376b98f9a65
desirekaleba <desirekaleba@gmail.com>
2020-03-31 17:35:06 +0300

    appending child

--- DOM/app.js
+++ DOM/app.js
@@ -26,13 +26,31 @@
 // let paragraphs = document.body.getElementsByTagName("p");
 // document.body.insertBefore(paragraphs[2], paragraphs[0]);
 
-function replaceImages() {
-    let images = document.body.getElementsByTagName("img");
-    for (let i = images.length -1; i >= 0; i--) {
-        let image = images[i];
-        if (image.alt) {
-            let text = document.createTextNode(image.alt);
-            image.parentNode.replaceChild(text, image);
-        }
+// function replaceImages() {
+//     let images = document.body.getElementsByTagName("img");
+//     for (let i = images.length -1; i >= 0; i--) {
+//         let image = images[i];
+//         if (image.alt) {
+//             let text = document.createTextNode(image.alt);
+//             image.parentNode.replaceChild(text, image);
+//         }
+//     }
+// }
+
+function elt(type, ...children) {
+    let node = document.createElement(type);
+    for (let child of children) {
+        if (typeof child != "string")
+            node.appendChild(child);
+        else
+            node.appendChild(document.createTextNode(child));
     }
+    return node;
 }
+
+document.getElementById("quote").appendChild(elt("footer", "-",
+                                                elt("strong", "Desire Kaleba"),
+                                                ", preface to the second edition of ",
+                                                elt("em", "The Open mind"),
+                                                ", 2003"
+                                                ));
