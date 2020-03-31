a6a5e3104eed1064a9c2d1e877f2252f4b24558a
desirekaleba <desirekaleba@gmail.com>
2020-03-31 16:07:20 +0300

    creating nodes

--- DOM/app.js
+++ DOM/app.js
@@ -23,5 +23,16 @@
 
 // let ostrich_img = document.getElementsByClassName("img")[0];
 // console.log(ostrich_img);
-let paragraphs = document.body.getElementsByTagName("p");
-document.body.insertBefore(paragraphs[2], paragraphs[0]);
+// let paragraphs = document.body.getElementsByTagName("p");
+// document.body.insertBefore(paragraphs[2], paragraphs[0]);
+
+function replaceImages() {
+    let images = document.body.getElementsByTagName("img");
+    for (let i = images.length -1; i >= 0; i--) {
+        let image = images[i];
+        if (image.alt) {
+            let text = document.createTextNode(image.alt);
+            image.parentNode.replaceChild(text, image);
+        }
+    }
+}
