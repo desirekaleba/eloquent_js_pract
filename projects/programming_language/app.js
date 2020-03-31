1e7fdb990a2b91108752cb52af9509d30aa9696f
desirekaleba <desirekaleba@gmail.com>
2020-03-31 13:35:45 +0300

    fixing scope, exercise

--- projects/programming_language/app.js
+++ projects/programming_language/app.js
@@ -240,3 +240,31 @@ print(f(4)(5)))
 `);
 
 console.log(parse("// hello i am the 1st comment type\nx\n# i am the second comment type"));
+
+// fixing scope
+specialForms.set = (args, env) => {
+    if (args.length != 2 || args[0].type != "word") {
+        throw new SyntaxError("Bad use of set");
+    }
+    let varName = args[0].name;
+    let value = evaluate(args[1], env);
+
+    for (let scope = env; scope; scope = Object.getPrototypeOf(scope)) {
+        if (Object.prototype.hasOwnProperty.call(scope, varName)) {
+            scope[varName] = value;
+            return value;
+        }
+    }
+    throw new ReferenceError(`Setting undefined variable ${varName}`);
+};
+
+run(`
+do(define(x, 4),
+    define(setx, fun(val, set(x, val))),
+    setx(50),
+    print(x))
+`);
+
+// run(`
+// set(quux, true)
+// `);
