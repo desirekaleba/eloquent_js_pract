f7253156b598bb9f78b20988bb1e8c0b47be3d2d
desirekaleba <desirekaleba@gmail.com>
2020-03-31 15:30:19 +0300

    getElementsByTagName

--- DOM/app.js
+++ DOM/app.js
@@ -1,14 +1,18 @@
 function talksAbout (node, string) {
     if (node.nodeType == Node.ELEMENT_NODE) {
-        for (let i = 0; i < node.children.length; i++) {
+        for (let i = 0; i < node.childNodes.length; i++) {
             if (talksAbout(node.childNodes[i], string)) {
                 return true;
             }
         }
         return false;
         
-    } else if (node.NodeType == Node.TEXT_NODE) {
+    } else if (node.nodeType == Node.TEXT_NODE) {
         return node.nodeValue.indexOf(string) > -1;
     }
 }
-console.log(talksAbout(document.body, "book"))
+console.log(talksAbout(document.body, "book"));
+
+// getElementsByTagName
+let link = document.getElementsByTagName("a")[0];
+console.log(link.href);
