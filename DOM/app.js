a0cbd7f334e40b35b16bce3cc0419bc65d16d393
desirekaleba <desirekaleba@gmail.com>
2020-03-31 15:53:04 +0300

    changing the document

--- DOM/app.js
+++ DOM/app.js
@@ -1,25 +1,27 @@
-function talksAbout (node, string) {
-    if (node.nodeType == Node.ELEMENT_NODE) {
-        for (let i = 0; i < node.childNodes.length; i++) {
-            if (talksAbout(node.childNodes[i], string)) {
-                return true;
-            }
-        }
-        return false;
+// function talksAbout (node, string) {
+//     if (node.nodeType == Node.ELEMENT_NODE) {
+//         for (let i = 0; i < node.childNodes.length; i++) {
+//             if (talksAbout(node.childNodes[i], string)) {
+//                 return true;
+//             }
+//         }
+//         return false;
         
-    } else if (node.nodeType == Node.TEXT_NODE) {
-        return node.nodeValue.indexOf(string) > -1;
-    }
-}
-console.log(talksAbout(document.body, "book"));
+//     } else if (node.nodeType == Node.TEXT_NODE) {
+//         return node.nodeValue.indexOf(string) > -1;
+//     }
+// }
+// console.log(talksAbout(document.body, "book"));
 
-// getElementsByTagName
-let link = document.getElementsByTagName("a")[0];
-console.log(link.href);
+// // getElementsByTagName
+// let link = document.getElementsByTagName("a")[0];
+// console.log(link.href);
 
-// getElementById
-let ostrich = document.getElementById("ostrich");
-console.log(ostrich);
+// // getElementById
+// let ostrich = document.getElementById("ostrich");
+// console.log(ostrich);
 
-let ostrich_img = document.getElementsByClassName("img")[0];
-console.log(ostrich_img);
+// let ostrich_img = document.getElementsByClassName("img")[0];
+// console.log(ostrich_img);
+let paragraphs = document.body.getElementsByTagName("p");
+document.body.insertBefore(paragraphs[2], paragraphs[0]);
