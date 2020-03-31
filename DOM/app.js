5b99d4093bac16877efba1adff862ef72d29a51e
desirekaleba <desirekaleba@gmail.com>
2020-04-01 00:22:35 +0300

    position and animation

--- DOM/app.js
+++ DOM/app.js
@@ -67,21 +67,34 @@
 // console.log("clientHeight:", para.clientHeight);
 // console.log("offsetHeight:", para.offsetHeight);
 
-function time(name, action) {
-    let start = Date.now();
-    action();
-    console.log(name, "took", Date.now() - start, "ms");
-}
-time("naive", () => {
-    let target = document.getElementById("one");
-    while (target.offsetWidth < 2000) {
-        target.appendChild(document.createTextNode("X"));
-    }
-});
+// function time(name, action) {
+//     let start = Date.now();
+//     action();
+//     console.log(name, "took", Date.now() - start, "ms");
+// }
+// time("naive", () => {
+//     let target = document.getElementById("one");
+//     while (target.offsetWidth < 2000) {
+//         target.appendChild(document.createTextNode("X"));
+//     }
+// });
 
-time("clever", function() {
-    let target = document.getElementById("two");
-    target.appendChild(document.createTextNode("XXXXX"));
-    let total = Math.ceil(2000 / (target.offsetWidth / 5));
-    target.firstChild.nodeValue = "X".repeat(total);
-});
+// time("clever", function() {
+//     let target = document.getElementById("two");
+//     target.appendChild(document.createTextNode("XXXXX"));
+//     let total = Math.ceil(2000 / (target.offsetWidth / 5));
+//     target.firstChild.nodeValue = "X".repeat(total);
+// });
+
+// Position and animation
+let cat = document.querySelector("img");
+let angle = Math.PI / 2;
+function animate(time, lastTime) {
+    if (lastTime =! null) {
+        angle += (time - lastTime) * 0.001;
+    }
+    cat.style.top = (Math.sin(angle) * 20) + "px";
+    cat.style.left = (Math.cos(angle) * 200) + "px";
+    requestAnimationFrame(newTime => animate(newTime, time));
+}
+requestAnimationFrame(animate);
