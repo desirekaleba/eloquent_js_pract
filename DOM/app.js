78e6e330ce5b878cdbe5f6db0d516c0ca26c6869
desirekaleba <desirekaleba@gmail.com>
2020-04-01 18:59:47 +0300

    Scroll Events, a progress bar above the document and updates

--- DOM/app.js
+++ DOM/app.js
@@ -311,22 +311,34 @@
 // }
 
 // Touch events
-function update(event) {
-    for (let dot; dot = document.querySelector("dot");) {
-        dot.remove();
-    }
-    for (let i = 0; i < event.touches.length; i++) {
-        let {
-            pageX,
-            pageY
-        } = event.touches[i];
-        let dot = document.createElement("dot");
-        dot.style.left = (pageX - 50) + "px";
-        dot.style.top = (pageY - 50) + "px";
-        document.body.appendChild(dot);
-        252
-    }
-}
-window.addEventListener("touchstart", update);
-window.addEventListener("touchmove", update);
-window.addEventListener("touchend", update);
+// function update(event) {
+//     for (let dot; dot = document.querySelector("dot");) {
+//         dot.remove();
+//     }
+//     for (let i = 0; i < event.touches.length; i++) {
+//         let {
+//             pageX,
+//             pageY
+//         } = event.touches[i];
+//         let dot = document.createElement("dot");
+//         dot.style.left = (pageX - 50) + "px";
+//         dot.style.top = (pageY - 50) + "px";
+//         document.body.appendChild(dot);
+//         252
+//     }
+// }
+// window.addEventListener("touchstart", update);
+// window.addEventListener("touchmove", update);
+// window.addEventListener("touchend", update);
+
+// Scroll Events
+// Create some content
+document.body.appendChild(document.createTextNode(
+    "DESIRE KALEBA".repeat(1000)
+));
+
+let bar = document.querySelector("#progress");
+window.addEventListener("scroll", () => {
+    let maxHeight = document.body.scrollHeight - innerHeight;
+    bar.style.width = `${(pageYOffset / maxHeight) * 100}%`;
+});
