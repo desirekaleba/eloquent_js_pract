cb274dd782aa761caa2912e00df6854f818f5af0
desirekaleba <desirekaleba@gmail.com>
2020-04-03 13:17:53 +0300

    State

--- projects/platform_game/src/main.js
+++ projects/platform_game/src/main.js
@@ -17,3 +17,19 @@ class Level {
         });
     }
 }
+
+class State {
+    constructor(level, actors, status) {
+        this.level = level;
+        this.actors = actors;
+        this.status = status;
+    }
+
+    static start(level) {
+        return new State(level, level.startActors, "playing");
+    }
+
+    get player() {
+        return this.actors.find(a => a.type == "player");
+    }
+}
