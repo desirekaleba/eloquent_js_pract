9e76c490e60a909d48bb673909c902c84438de93
desirekaleba <desirekaleba@gmail.com>
2020-04-03 14:31:38 +0300

    drawGrid func init, draw level

--- projects/platform_game/src/main.js
+++ projects/platform_game/src/main.js
@@ -158,3 +158,13 @@ class DOMDisplay {
         this.dom.remove();
     }
 }
+
+const scale = 20;
+
+function drawGrid(level) {
+    return elt("table", {
+        class: "background",
+        style: `width: ${level.width * scale}px`
+    }, ...level.rows.map(row => elt("tr", {style: `height: ${scale}px`},
+                            ...row.map(type => elt("td", {class: type})))));
+}
