4795ceabe462ea1221c79e624cef219776370258
desirekaleba <desirekaleba@gmail.com>
2020-04-03 17:13:27 +0300

    Lava update method init

--- projects/platform_game/src/main.js
+++ projects/platform_game/src/main.js
@@ -276,3 +276,14 @@ Coin.prototype.collide = function(state) {
         status = "won";
     return new State(state.level, filtered, status);
 };
+
+Lava.prototype.update = function(time, state) {
+    let newPos = this.pos.plus(this.speed.times(time));
+    if (!state.level.touches(newPos, this.size, "wall")) {
+        return new Lava(newPos, this.speed, this.reset);
+    } else if (this.reset) {
+        return new Lava(this.reset, this.speed, this.reset);
+    } else {
+        return new Lava(this.pos, this.speed.times(-1));
+    }
+};
