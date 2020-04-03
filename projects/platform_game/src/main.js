1256fd29f8b4860665d8fc2fa396172bf1429b44
desirekaleba <desirekaleba@gmail.com>
2020-04-03 18:02:14 +0300

    runLevel, function to run levels

--- projects/platform_game/src/main.js
+++ projects/platform_game/src/main.js
@@ -355,3 +355,25 @@ function runAnimation(frameFunc) {
     }
     requestAnimationFrame(frame);
 }
+
+function runLevel(level, Display) {
+    let display = new Display(document.body, level);
+    let state = State.start(level);
+    let ending = 1;
+    return new Promise(resolve => {
+        runAnimation(time => {
+            state = state.update(time, arrowKeys);
+            display.syncState(state);
+            if (state.status == "playing") {
+                return true;
+            } else if (ending > 1) {
+                ending -= time;
+                return true;
+            } else {
+                display.clear();
+                resolve(state.status);
+                return false;
+            }
+        });
+    });
+}
