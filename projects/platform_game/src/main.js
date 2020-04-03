8b929c14b47c129366576ed474d121004e04ce23
desirekaleba <desirekaleba@gmail.com>
2020-04-03 13:35:19 +0300

    Player Class init

--- projects/platform_game/src/main.js
+++ projects/platform_game/src/main.js
@@ -48,3 +48,20 @@ class Vec {
         return new Vec(this.x * factor, this.y * factor);
     }
 }
+
+class Player {
+    constructor(pos, speed) {
+        this.pos = pos;
+        this.speed = speed;
+    }
+
+    get type() {
+        return "player";
+    }
+
+    static create(pos) {
+        return new Player(pos.plus(new Vec(0, -0.5)), new Vec(0, 0));
+    }
+}
+
+Player.prototype.size = new Vec(0.8, 1.5);
