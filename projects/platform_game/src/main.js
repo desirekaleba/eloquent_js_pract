7a0132297da2cac5e18ab4b206ab93e3a99871e9
desirekaleba <desirekaleba@gmail.com>
2020-04-03 14:20:46 +0300

    DOMDisplay class init, a way to draw a given level and state

--- projects/platform_game/src/main.js
+++ projects/platform_game/src/main.js
@@ -146,3 +146,15 @@ function elt(name, attrs, ...children) {
     }
     return dom;
 }
+
+class DOMDisplay {
+    constructor(parent, level) {
+        this.dom = elt("div", {class: "game"}, drawGrid(level));
+        this.actorLayer = null;
+        parent.appendChild(this.dom);
+    }
+
+    clear() {
+        this.dom.remove();
+    }
+}
