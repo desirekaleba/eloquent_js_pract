6994b4361a7c3e92c03583429d1861390cbcf37a
desirekaleba <desirekaleba@gmail.com>
2020-04-03 16:51:14 +0300

    update, figures out whether the player is touching lava

--- projects/platform_game/src/main.js
+++ projects/platform_game/src/main.js
@@ -235,3 +235,25 @@ Level.prototype.touches = function(pos, size, type) {
     }
     return false;
 };
+
+State.prototype.update = function(time, keys) {
+    let actors = this.actors
+        .map(actor => actor.update(time, this, keys));
+    let newState = new State(this.level, actors, this.status);
+
+    if (newState.status != "playing")
+        return newState;
+
+    let player = newState.player;
+
+    if (this.level.touches(player.pos, player.size, "lava")) {
+        return new State(this.level, actors, "lost");
+    }
+
+    for (let actor of actors) {
+        if (actor != player && overlap(actor, player)) {
+            newState = actor.collide(newState);
+        }
+    }
+    return newState;
+};
