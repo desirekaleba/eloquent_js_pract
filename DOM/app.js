4c71cc08e2e534a39d01ad33682d99bff6c1d8b7
desirekaleba <desirekaleba@gmail.com>
2020-04-05 08:46:19 +0300

    SVG

--- DOM/app.js
+++ DOM/app.js
@@ -443,31 +443,35 @@
 // });
 
 // Tabs
-function asTabs(node) {
-    let tabs = Array.from(node.children).map(node => {
-        let button = document.createElement("button");
-        button.textContent = node.getAttribute("data-tabname");
-        let tab = {
-            node,
-            button
-        };
-        button.addEventListener("click", () => selectTab(tab));
-        return tab;
-    });
+// function asTabs(node) {
+//     let tabs = Array.from(node.children).map(node => {
+//         let button = document.createElement("button");
+//         button.textContent = node.getAttribute("data-tabname");
+//         let tab = {
+//             node,
+//             button
+//         };
+//         button.addEventListener("click", () => selectTab(tab));
+//         return tab;
+//     });
 
-    let tabList = document.createElement("div");
-    for (let {button} of tabs) {
-        tabList.appendChild(button);
-    }
-    node.insertBefore(tabList, node.firstChild);
+//     let tabList = document.createElement("div");
+//     for (let {button} of tabs) {
+//         tabList.appendChild(button);
+//     }
+//     node.insertBefore(tabList, node.firstChild);
 
-    function selectTab(selectedTab) {
-        for (let tab of tabs) {
-            let selected = tab == selectedTab;
-            tab.node.style.display = selected ? "" : "none";
-            tab.button.style.color = selected ? "red" : "";
-        }
-    }
-    selectTab(tabs[0]);
-}
-asTabs(document.querySelector("tab-panel"));
+//     function selectTab(selectedTab) {
+//         for (let tab of tabs) {
+//             let selected = tab == selectedTab;
+//             tab.node.style.display = selected ? "" : "none";
+//             tab.button.style.color = selected ? "red" : "";
+//         }
+//     }
+//     selectTab(tabs[0]);
+// }
+// asTabs(document.querySelector("tab-panel"));
+
+// SVG
+let circle = document.querySelector("circle");
+circle.setAttribute("fill", "cyan");
