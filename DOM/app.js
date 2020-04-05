a48a707b29e93330cb0a830d6f03956b5903a176
desirekaleba <desirekaleba@gmail.com>
2020-04-05 11:52:47 +0300

    canvas, pie chart

--- DOM/app.js
+++ DOM/app.js
@@ -515,10 +515,49 @@
 // cx.closePath();
 // cx.stroke();
 
-let cx = document.getElementsByTagName("canvas")[0].getContext("2d");
-cx.beginPath();
-// center = (50, 50) radius=40 angle = 0 t0 7
-cx.arc(50, 50, 40, 0, 7);
-// center = (150, 50) radius = 40 angle = 0 t0 π½
-cx.arc(150, 50, 40, 0, 0.5 * Math.PI);
-cx.stroke();
+// let cx = document.getElementsByTagName("canvas")[0].getContext("2d");
+// cx.beginPath();
+// // center = (50, 50) radius=40 angle = 0 t0 7
+// cx.arc(50, 50, 40, 0, 7);
+// // center = (150, 50) radius = 40 angle = 0 t0 π½
+// cx.arc(150, 50, 40, 0, 0.5 * Math.PI);
+// cx.stroke();
+
+// pie chart
+const results = [
+    {
+        name: "Satisfied",
+        count: 1043,
+        color: "lightblue"
+    },
+    {
+        name: "Neutral",
+        count: 563,
+        color: "lightgreen"
+    },
+    {
+        name: "Unsatisfied",
+        count: 510,
+        color: "pink"
+    },
+    {
+        name: "No comment",
+        count: 175,
+        color: "silver"
+    }
+];
+
+let cx = document.querySelector("canvas").getContext("2d");
+let total = results.reduce((sum, {count}) => sum + count, 0);
+
+let currentAngle = -0.5 * Math.PI;
+
+for (let result of results) {
+    let sliceAngle = (result.count / total) * 2 * Math.PI;
+    cx.beginPath();
+    cx.arc(100, 100, 100, currentAngle, currentAngle + sliceAngle);
+    currentAngle += sliceAngle;
+    cx.lineTo(100, 100);
+    cx.fillStyle = result.color;
+    cx.fill();
+}
