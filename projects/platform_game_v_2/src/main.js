43ccccbf3f219f1ba9d343db074ddd250823e506
desirekaleba <desirekaleba@gmail.com>
2020-04-06 09:11:03 +0300

    CanvasDisplay drawPlayer method attached

--- projects/platform_game_v_2/src/main.js
+++ projects/platform_game_v_2/src/main.js
@@ -252,6 +252,30 @@ CanvasDisplay.prototype.drawBackground = function(level) {
 
 let playerSprites = document.createElement("img");
 playerSprites.src = "../images/player.png";
+const playerXOverlap = 4;
+
+CanvasDisplay.prototype.drawPlayer = function(player, x, y, width, height) {
+    width += playerXOverlap * 2;
+    x -= playerXOverlap;
+    if (player.speed.x != 0) {
+        this.flipPlayer = player.speed.x < 0;
+    }
+    let tile = 8;
+    if (player.speed.y != 0) {
+        tile = 9;
+    } else if (player.speed.x != 0) {
+        tile = Math.floor(Date.now() / 60) % 8;
+    }
+
+    this.cx.save();
+    if (this.flipPlayer) {
+        flipHorizontally(this.cx, x + width / 2);
+    }
+
+    let tileX = tile * width;
+    this.cx.drawImage(playerSprites, tileX, 0, width, height, x, y, width, height);
+    this.cx.restore();
+};
 
 function drawActors(actors) {
     return elt("div", {}, ...actors.map(actor => {
