605303bcadd27d597bc43d76496b8ac58f072b38
desirekaleba <desirekaleba@gmail.com>
2020-04-06 22:22:02 +0300

    fetch api with headers option

--- app.js
+++ app.js
@@ -8,7 +8,11 @@
 //     .then(res => res.text())
 //     .then(data => console.log(data));
 
-fetch("example/data.txt", {method: "DELETE"})
-    .then(res => {
-        console.log(res.status);
-    });
+// fetch("example/data.txt", {method: "DELETE"})
+//     .then(res => {
+//         console.log(res.status);
+//     });
+
+fetch("example/data.txt", {headers: {Range: "bytes=8-19"}})
+    .then(res => res.text())
+    .then(console.log);
