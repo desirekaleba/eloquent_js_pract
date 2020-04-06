d0bca14dfc8103bd1727dbd733e4637fce35fa2a
desirekaleba <desirekaleba@gmail.com>
2020-04-07 01:11:22 +0300

    html page reader

--- app.js
+++ app.js
@@ -17,21 +17,80 @@
 //     .then(res => res.text())
 //     .then(console.log);
 
-let textarea = document.querySelector("textarea");
-textarea.addEventListener("keydown", event => {
-    if (event.keyCode == 113) {
-        replaceSelectionWith(textarea, "DESKA");
-        event.preventDefault();
+// let textarea = document.querySelector("textarea");
+// textarea.addEventListener("keydown", event => {
+//     if (event.keyCode == 113) {
+//         replaceSelectionWith(textarea, "DESKA");
+//         event.preventDefault();
+//     }
+
+// });
+
+// function replaceSelectionWith(field, word) {
+//     let from = field.selectionStart,
+//         to = field.selectionEnd;
+//     field.value = field.value.slice(0, from) + word +
+//         field.value.slice(to);
+//     // Put the cursor after the word
+//     field.selectionStart = from + word.length;
+//     field.selectionEnd = from + word.length;
+// }
+
+// let input = document.querySelector("input");
+// let output = document.querySelector("#length");
+
+// input.addEventListener("input", event => {
+//     output.textContent = input.value.length;
+// });
+
+// let purple = document.querySelector("#purple");
+// purple.addEventListener("change", () => {
+//     document.body.style.backgroundColor = purple.checked ? "mediumpurple" : "";
+// });
+
+// let buttons = document.querySelectorAll("input[name=color]");
+// for (let button of Array.from(buttons)) {
+//     button.addEventListener("change", () => {
+//         document.body.style.backgroundColor = button.value;
+//     });
+// }
+
+// let select = document.querySelector("select");
+// let output = document.querySelector("#output");
+// let number = 0;
+
+// select.addEventListener("change", () => {
+//     for (let option of Array.from(select.options)) {
+//         if (option.selected) {
+//             number += Number(option.value);
+//         }
+//     }
+//     output.textContent = number;
+// });
+
+// let file = document.querySelector("input");
+// file.addEventListener("change", () => {
+//     if (file.files.length > 0) {
+//         let f = file.files[0];
+//         console.log("You Chose", f.name, ", has", Math.ceil((f.size / 1024) / 1024), "of size in MB", "and was last modified on", new Date(f.lastModified));
+//         if (f.type)
+//             console.log("it has type", f.type);
+//     }
+// });
+
+// reading a file
+let input_file = document.querySelector("input");
+let output = document.querySelector("#output");
+
+input_file.addEventListener("change", () => {
+    for (let file of Array.from(input_file.files)) {
+        let reader = new FileReader();
+        reader.addEventListener("load", () => {
+            if (file.type != "text/html")
+            console.log("Not an html file");
+            else
+            output.textContent = reader.result;
+        });
+        reader.readAsText(file);
     }
-
 });
-
-function replaceSelectionWith(field, word) {
-    let from = field.selectionStart,
-        to = field.selectionEnd;
-    field.value = field.value.slice(0, from) + word +
-        field.value.slice(to);
-    // Put the cursor after the word
-    field.selectionStart = from + word.length;
-    field.selectionEnd = from + word.length;
-}
