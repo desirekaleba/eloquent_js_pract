031e2b64fd20a7932f93f257a597aa3c799f868e
desirekaleba <desirekaleba@gmail.com>
2020-04-07 00:03:47 +0300

    insert a word in a textarea using F2

--- app.js
+++ app.js
@@ -13,6 +13,25 @@
 //         console.log(res.status);
 //     });
 
-fetch("example/data.txt", {headers: {Range: "bytes=8-19"}})
-    .then(res => res.text())
-    .then(console.log);
+// fetch("example/data.txt", {headers: {Range: "bytes=8-19"}})
+//     .then(res => res.text())
+//     .then(console.log);
+
+let textarea = document.querySelector("textarea");
+textarea.addEventListener("keydown", event => {
+    if (event.keyCode == 113) {
+        replaceSelectionWith(textarea, "DESKA");
+        event.preventDefault();
+    }
+
+});
+
+function replaceSelectionWith(field, word) {
+    let from = field.selectionStart,
+        to = field.selectionEnd;
+    field.value = field.value.slice(0, from) + word +
+        field.value.slice(to);
+    // Put the cursor after the word
+    field.selectionStart = from + word.length;
+    field.selectionEnd = from + word.length;
+}
