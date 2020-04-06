46a2e7f82f2ca2ed970c0d03d4f469513b9e30d8
desirekaleba <desirekaleba@gmail.com>
2020-04-06 09:42:37 +0300

    CanvasDisplay drawActors method attached and can now run the new version

--- projects/platform_game_v_2/src/main.js
+++ projects/platform_game_v_2/src/main.js
@@ -169,6 +169,12 @@ function drawGrid(level) {
 
 
 // Canvas Display
+
+function flipHorizontally(context, around) {
+    context.translate(around, 0);
+    context.scale(-1, 1);
+    context.translate(-around, 0);
+}
 class CanvasDisplay {
     constructor(parent, level) {
         this.canvas = document.createElement("canvas");
@@ -227,8 +233,8 @@ CanvasDisplay.prototype.clearDisplay = function(status) {
     this.cx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 };
 
-let otherSprites = document.createElement("img");
-otherSprites.src = "../images/sprites.png";
+var otherSprites = document.createElement("img");
+otherSprites.src = "../res/images/sprites.png";
 
 CanvasDisplay.prototype.drawBackground = function(level) {
     let {left, top, width, height} = this.viewport;
@@ -244,14 +250,14 @@ CanvasDisplay.prototype.drawBackground = function(level) {
                 continue;
             let screenX = (x - left) * scale;
             let screenY = (y - top) * scale;
-            let tileX = tile == "lave" ? scale : 0;
+            let tileX = tile == "lava" ? scale : 0;
             this.cx.drawImage(otherSprites, tileX, 0, scale, scale, screenX, screenY, scale, scale);
         }
     }
 };
 
 let playerSprites = document.createElement("img");
-playerSprites.src = "../images/player.png";
+playerSprites.src = "../res/images/player.png";
 const playerXOverlap = 4;
 
 CanvasDisplay.prototype.drawPlayer = function(player, x, y, width, height) {
@@ -276,6 +282,23 @@ CanvasDisplay.prototype.drawPlayer = function(player, x, y, width, height) {
     this.cx.drawImage(playerSprites, tileX, 0, width, height, x, y, width, height);
     this.cx.restore();
 };
+
+CanvasDisplay.prototype.drawActors = function(actors) {
+    for (let actor of actors) {
+        let width = actor.size.x * scale;
+        let height = actor.size.y * scale;
+        let x = (actor.pos.x - this.viewport.left) * scale;
+        let y = (actor.pos.y - this.viewport.top) * scale;
+
+        if (actor.type == "player") {
+            this.drawPlayer(actor, x, y, width, height);
+        } else {
+            let tileX = (actor.type == "coin" ? 2 : 1) * scale;
+            this.cx.drawImage(otherSprites, tileX, 0, width, height, x, y, width, height);
+        }
+    }
+};
+
 
 function drawActors(actors) {
     return elt("div", {}, ...actors.map(actor => {
@@ -555,4 +578,4 @@ async function runGame(plans, Display) {
 
 import GAME_LEVELS from "./levels/levels.js";
 
-runGame(GAME_LEVELS, DOMDisplay);
+runGame(GAME_LEVELS, CanvasDisplay);
