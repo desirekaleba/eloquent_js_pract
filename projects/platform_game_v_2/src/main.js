a696a72f5f2a4bb08926a876e66a2c869e43749f
desirekaleba <desirekaleba@gmail.com>
2020-04-06 08:15:27 +0300

    CanvasDisplay object init

--- projects/platform_game_v_2/src/main.js
+++ projects/platform_game_v_2/src/main.js
@@ -167,6 +167,31 @@ function drawGrid(level) {
                             ...row.map(type => elt("td", {class: type})))));
 }
 
+
+// Canvas Display
+class CanvasDisplay {
+    constructor(parent, level) {
+        this.canvas = document.createElement("canvas");
+        this.canvas.width = Math.min(600, level.width * scale);
+        this.canvas.height = Math.min(450, level.height * scale);
+        parent.appendChild(this.canvas);
+        this.cx = this.canvas.getContext("2d");
+
+        this.flipPlayer = false;
+
+        this.viewport = {
+            left: 0,
+            top: 0,
+            width: this.canvas.width / scale,
+            height: this.canvas.height / scale
+        };
+    }
+
+    clear () {
+        this.canvas.remove();
+    }
+}
+
 function drawActors(actors) {
     return elt("div", {}, ...actors.map(actor => {
         let rect = elt("div", {class: `actor ${actor.type}`});
