1f1eb1fef007572cb968e7782196b39a7d4061b9
desirekaleba <desirekaleba@gmail.com>
2020-04-07 14:13:41 +0300

    crun

--- app.js
+++ app.js
@@ -102,59 +102,59 @@
 // console.log(localStorage.getItem("username"));
 
 // crude note-taking application
-let list = document.querySelector("select");
-let note = document.querySelector("textarea");
+// let list = document.querySelector("select");
+// let note = document.querySelector("textarea");
 
-let state;
+// let state;
 
-function setState(newState) {
-    list.textContent = "";
-    for (let name of Object.keys(newState.notes)) {
-        let option = document.createElement("option");
-        option.textContent = name;
-        if (newState.selected == name)
-            option.selected == true;
-        list.appendChild(option);
-    }
-    note.value = newState.notes[newState.selected];
+// function setState(newState) {
+//     list.textContent = "";
+//     for (let name of Object.keys(newState.notes)) {
+//         let option = document.createElement("option");
+//         option.textContent = name;
+//         if (newState.selected == name)
+//             option.selected == true;
+//         list.appendChild(option);
+//     }
+//     note.value = newState.notes[newState.selected];
 
-    localStorage.setItem("Notes", JSON.stringify(newState));
-    state = newState;
-}
+//     localStorage.setItem("Notes", JSON.stringify(newState));
+//     state = newState;
+// }
 
-setState(JSON.parse(localStorage.getItem("Notes")) || {
-    notes: {
-        "shopping list": "Carrots\nRaisins"
-    },
-    selected: "shopping list"
-});
+// setState(JSON.parse(localStorage.getItem("Notes")) || {
+//     notes: {
+//         "shopping list": "Carrots\nRaisins"
+//     },
+//     selected: "shopping list"
+// });
 
-list.addEventListener("change", () => {
-    setState({notes: state.notes, selected: list.value});
-});
+// list.addEventListener("change", () => {
+//     setState({notes: state.notes, selected: list.value});
+// });
 
-note.addEventListener("change", () => {
-    setState({
-        notes: Object.assign({}, state.notes, 
-            {[state.selected]: note.value}),
-        selected: state.selected
-    });
-});
+// note.addEventListener("change", () => {
+//     setState({
+//         notes: Object.assign({}, state.notes, 
+//             {[state.selected]: note.value}),
+//         selected: state.selected
+//     });
+// });
 
-document.querySelector("#add")
-    .addEventListener("click", () => {
-        let name = prompt("Note name");
-        if (name) {
-            setState({
-                notes: Object.assign({}, state.notes, {[name]: ""}),
-                selected: name
-            });
-        }
-    });
+// document.querySelector("#add")
+//     .addEventListener("click", () => {
+//         let name = prompt("Note name");
+//         if (name) {
+//             setState({
+//                 notes: Object.assign({}, state.notes, {[name]: ""}),
+//                 selected: name
+//             });
+//         }
+//     });
 
-document.querySelector("#delete")
-    .addEventListener("click", () => {
-        localStorage.removeItem("Notes");
-        list.textContent = "";
-        note.value = "";
-    });
+// document.querySelector("#delete")
+//     .addEventListener("click", () => {
+//         localStorage.removeItem("Notes");
+//         list.textContent = "";
+//         note.value = "";
+//     });
