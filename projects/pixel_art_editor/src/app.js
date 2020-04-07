4a168fe6da90c6b910d5fb958f8a9b7336cfbfd8
desirekaleba <desirekaleba@gmail.com>
2020-04-07 20:56:00 +0300

    application, PixelEditor interface, ini

--- projects/pixel_art_editor/src/app.js
+++ projects/pixel_art_editor/src/app.js
@@ -119,3 +119,30 @@ PictureCanvas.prototype.touch = function(startEvent, onDown) {
     this.dom.addEventListener("touchmove", move);
     this.dom.addEventListener("touchend", end);
 };
+
+// The application
+class PixelEditor {
+    constructor(state, config) {
+        let {tools, controls, dispatch} = config;
+        this.state = state;
+
+        this.canvas = new PictureCanvas(state.picture, pos => {
+            let tool = tools[this.state.tool];
+            let onMove = tool(pos, this.state, dispatch);
+            if (onMove)
+                return pos => onMove(pos, this.state);
+        });
+        this.controls = controls.map(control => new control(state, config));
+        this.dom = elt("div", {}, this.canvas.dom, elt("br"),
+            ...this.controls.reduce(
+                (a, c) => a.concat(" ", c.dom),
+                []
+            ));
+    }
+    syncState(state) {
+        this.state = state;
+        this.canvas.syncState(state.picture);
+        for (let ctrl of this.controls)
+            ctrl.syncState(state);
+    }
+}
