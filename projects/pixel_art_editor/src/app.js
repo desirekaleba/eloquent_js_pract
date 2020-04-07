d5e0b913645db3c28243b85654ce7d144946aa1a
desirekaleba <desirekaleba@gmail.com>
2020-04-07 22:34:25 +0300

    SaveButton, an interface that saves the picture currently working on

--- projects/pixel_art_editor/src/app.js
+++ projects/pixel_art_editor/src/app.js
@@ -285,3 +285,27 @@ function fill({
 function pick(pos, state, dispatch) {
     dispatch({color: state.picture.pixel(pos.x, pos.y)});
 }
+
+class SaveButton {
+    constructor(state) {
+        this.picture = state.picture;
+        this.dom = elt("button", {
+            onclick: () => this.save()
+        }, "💾 Save");
+    }
+
+    save () {
+        let canvas = elt("canvas");
+        drawPicture(this.picture, canvas, 1);
+        let link = elt("a", {
+            href: canvas.toDataURL(),
+            download: "pixelart.png"
+        });
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+    }
+    syncState(state) {
+        this.picture = state.picture;
+    }
+}
