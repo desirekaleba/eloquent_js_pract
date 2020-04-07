db0f66703ad4fef520780edb2e49d65ae6a8c203
desirekaleba <desirekaleba@gmail.com>
2020-04-07 19:53:25 +0300

    elt function init, help to create a dom element by type, props and children

--- projects/pixel_art_editor/src/app.js
+++ projects/pixel_art_editor/src/app.js
@@ -26,3 +26,14 @@ class Picture {
 function updateState(state, action) {
     return Object.assign({}, state, action);
 }
+
+function elt(type, props, ...children) {
+    let dom = document.createElement(type);
+    if (props) Object.assign(dom, props);
+    for (let child of children) {
+        if (typeof child != "string")
+            dom.appendChild(child);
+        else dom.appendChild(document.createTextNode(child));
+    }
+    return dom;
+}
