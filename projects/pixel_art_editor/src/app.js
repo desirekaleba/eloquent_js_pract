1f87dbeaaa7dc71e3a6fc9a2c9aa3f1fee563927
desirekaleba <desirekaleba@gmail.com>
2020-04-08 21:25:14 +0300

    circle, a tool to draw cirles on the canvas added

--- projects/pixel_art_editor/src/app.js
+++ projects/pixel_art_editor/src/app.js
@@ -199,6 +199,29 @@ class PixelEditor {
     }
 }
 
+function circle(pos, state, dispatch) {
+    function drawCircles(to) {
+        let radius = Math.sqrt(Math.pow(to.x - pos.x, 2) +
+                                Math.pow(to.y - pos.y, 2));
+        let radiusC = Math.ceil(radius);
+        let drawn = [];
+        for (let dy = -radiusC; dy <=  radiusC; dy++) {
+            for (let dx = -radiusC; dx <= radiusC; dx++) {
+                let dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+                if (dist > radius)
+                    continue;
+                let y = pos.y + dy, x = pos.x + dx;
+                if (y < 0 || y >=  state.picture.height ||
+                    x < 0 || x >= state.picture.width)
+                        continue;
+                drawn.push({x, y, color: state.color});
+            }
+        }
+        dispatch({picture: state.picture.draw(drawn)});
+    }
+    drawCircles(pos);
+    return drawCircles;
+}
 class ToolSelect {
     constructor(state, {
         tools,
@@ -465,6 +488,11 @@ function startPixelEditor({state = startState, tools = baseTools, controls = bas
     return app.dom;
 }
 
+let dom = startPixelEditor({
+    tools: Object.assign({}, baseTools, {circle})
+});
 // getting it to the screen
-document.querySelector("div")
-    .appendChild(startPixelEditor({}));
+let div = document.querySelector("div");
+//div.appendChild(startPixelEditor({}));
+div.appendChild(dom);
+
