1d885fed144281c756e9ca9c23a512c5eff797df
desirekaleba <desirekaleba@gmail.com>
2020-04-10 00:42:38 +0300

    POST comments, a method to post comments on talks

--- projects/skill_sharing/server/app.js
+++ projects/skill_sharing/server/app.js
@@ -101,3 +101,32 @@ router.add("PUT", talkPath, async (server, title, request) => {
     server.updated();
     return {status: 204};
 });
+
+router.add("POST", /^\/talks\/([^\/]+)\/comments$/, async (server, title, request) => {
+    let requestBody = await readStream(request);
+    let comment;
+    try {
+        comment = JSON.parse(requestBody);
+    } catch (_) {
+        return {
+            status: 400,
+            body: "Invalid JSON"
+        };
+    }
+    if (!comment || typeof comment.author != "string" || 
+        typeof comment.message != "string") {
+            return {
+                status: 400,
+                body: "Bad comment data"
+            };
+        } else if (title in server.talks) {
+            server.talks[title].comments.push(comment);
+            server.updated();
+            return {status: 204};
+        } else {
+            return {
+                status: 404,
+                body: `Not talk matches ${title}`
+            };
+        }
+});
