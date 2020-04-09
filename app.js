b3cf0ec682b4b8dfa00f2f75950285697417e1c6
desirekaleba <desirekaleba@gmail.com>
2020-04-09 12:25:35 +0300

    file system, readFile as a buffer

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-let { readFile } = require("fs");
-readFile("file.txt", "utf-8", (err, text) => {
+// let { readFile } = require("fs");
+// readFile("file.txt", "utf-8", (err, text) => {
+//     if (err)
+//         throw err;
+//     console.log("the file contains:", text);
+// });
+
+// If you don't specify the character encoding,
+// node will assume you are interested in the
+// binary data and will give you a Buffer object
+const { readFile } = require("fs");
+readFile("file.txt", (err, buffer) => {
     if (err)
         throw err;
-    console.log("the file contains:", text);
+    console.log("The file contained: ", buffer.length, "bytes.",
+            "the first byte is:", buffer[0]);
 });
