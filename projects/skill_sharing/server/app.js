9b535a55195e192ad5d425c7b4b6f19ddb06c7fd
desirekaleba <desirekaleba@gmail.com>
2020-04-10 00:27:45 +0300

    PUT method add to the router, a way to add talks

--- projects/skill_sharing/server/app.js
+++ projects/skill_sharing/server/app.js
@@ -72,3 +72,32 @@ function readStream(stream) {
         stream.on("end", () => resolve(data));
     });
 }
+
+router.add("PUT", talkPath, async (server, title, request) => {
+    let requestBody = await readStream(request);
+    let talk;
+    try {
+        talk = JSON.parse(requestBody);
+    } catch (_) {
+        return {
+            status: 400,
+            body: "Invalid JSON"
+        };
+    }
+
+    if (!talk || typeof talk.presenter != "string" ||
+        typeof talk.summary != "string") {
+            return {
+                status: 400,
+                body: "Bad talk data"
+            };
+        }
+    server.talks[title] = {
+        title,
+        presenter: talk.presenter,
+        summary: talk.summary,
+        comments: []
+    };
+    server.updated();
+    return {status: 204};
+});
