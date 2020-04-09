0c20daee26fa76552b5723c764a7e2a1759e1d87
desirekaleba <desirekaleba@gmail.com>
2020-04-09 12:32:34 +0300

    file system, writeFile, a function to write to files

--- app.js
+++ app.js
@@ -8,10 +8,18 @@
 // If you don't specify the character encoding,
 // node will assume you are interested in the
 // binary data and will give you a Buffer object
-const { readFile } = require("fs");
-readFile("file.txt", (err, buffer) => {
+// const { readFile } = require("fs");
+// readFile("file.txt", (err, buffer) => {
+//     if (err)
+//         throw err;
+//     console.log("The file contained: ", buffer.length, "bytes.",
+//             "the first byte is:", buffer[0]);
+// });
+
+// writeFile
+const { writeFile } = require("fs");
+writeFile("file.txt", "Node has visited you", err => {
     if (err)
-        throw err;
-    console.log("The file contained: ", buffer.length, "bytes.",
-            "the first byte is:", buffer[0]);
+        console.log(`Failed to write file: ${err}`);
+    else console.log("File written");
 });
