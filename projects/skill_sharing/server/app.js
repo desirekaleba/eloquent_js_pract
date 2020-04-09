a188b4d614e778dd9af8ee2d0463ede9ee8a510c
desirekaleba <desirekaleba@gmail.com>
2020-04-10 00:17:10 +0300

    readStream, a method which reads request bodies

--- projects/skill_sharing/server/app.js
+++ projects/skill_sharing/server/app.js
@@ -63,3 +63,12 @@ router.add("DELETE", talkPath, async (server, title) => {
     }
     return { status: 204 };
 });
+
+function readStream(stream) {
+    return new Promise((resolve, reject) => {
+        let data = "";
+        stream.on("error", reject);
+        stream.on("data", chunk => data += chunk.toString());
+        stream.on("end", () => resolve(data));
+    });
+}
