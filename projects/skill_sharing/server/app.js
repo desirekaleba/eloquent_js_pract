a252171193d27907e6127bd7ca74da03ff260572
desirekaleba <desirekaleba@gmail.com>
2020-04-10 23:31:31 +0300

    Disk persistence, a way to store all the talks in a json file

--- projects/skill_sharing/server/app.js
+++ projects/skill_sharing/server/app.js
@@ -171,11 +171,29 @@ SkillShareServer.prototype.waitForChanges = function(time) {
     });
 };
 
+// using a json file to store data
+const { readFileSync, writeFile } = require("fs");
+
+const fileName = "./talks.json";
+
+function loadTalks() {
+    let json;
+    try {
+        json = JSON.parse(readFileSync(fileName, "UTF-8"));
+    } catch (e) {
+        json = {};
+    }
+    return Object.assign(Object.create(null), json);
+}
 SkillShareServer.prototype.updated = function() {
     this.version++;
     let response = this.talkResponse();
     this.waiting.forEach(resolve => resolve(response));
     this.waiting = [];
+
+    writeFile(fileName, JSON.stringify(this.talks), e => {
+        if (e) throw e;
+    });
 };
 
-new SkillShareServer(Object.create(null)).start(8000);
+new SkillShareServer(loadTalks()).start(8000);
