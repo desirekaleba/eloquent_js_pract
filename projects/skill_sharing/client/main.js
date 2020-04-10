0f548540a382a2348577b0c0290009a0f672925c
desirekaleba <desirekaleba@gmail.com>
2020-04-10 14:10:27 +0300

    SkillShareApp, the application interface

--- projects/skill_sharing/client/main.js
+++ projects/skill_sharing/client/main.js
@@ -134,3 +134,27 @@ async function pollTalks(update) {
         update(await response.json());
     }
 }
+
+class SkillShareApp {
+    constructor(state, dispatch) {
+        this.dispatch = dispatch,
+        this.talkDOM = elt("div", {className: "talks"});
+        this.dom = elt("div", null,
+                    renderUserField(state.user, dispatch),
+                    this.talkDOM,
+                    renderTalkForm(dispatch));
+        this.syncState(state);
+    }
+
+    syncState(state) {
+        if (state.talks != this.talks) {
+            this.talkDOM.textContent = "";
+            for (let talk of state.talks) {
+                this.talkDOM.appendChild(
+                    renderTalk(talk, this.dispatch)
+                );
+            }
+            this.talks = state.talks;
+        }
+    }
+}
