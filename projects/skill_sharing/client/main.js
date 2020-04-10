37cf7c202ce53bbb025df8fbeda82b73986cb932
desirekaleba <desirekaleba@gmail.com>
2020-04-10 14:03:53 +0300

    pollTalks, a way of polling the talks

--- projects/skill_sharing/client/main.js
+++ projects/skill_sharing/client/main.js
@@ -114,3 +114,23 @@ function renderTalkForm(dispatch) {
             type: "submit"
         }, "Submit"));
 }
+
+async function pollTalks(update) {
+    let tag = undefined;
+    for (;;) {
+        let response;
+        try {
+            response = await fetchOk("/talks", {
+                headers: tag && {"If-None-Match": tag, "Prefer": "wait=90"}
+            });
+        } catch (e) {
+            console.log("Request failed: " + e);
+            await new Promise(resolve => setTimeout(resolve, 500));
+            continue;
+        }
+        if (response.status == 304)
+            continue;
+        tag = response.headers.get("ETag");
+        update(await response.json());
+    }
+}
