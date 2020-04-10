874e31b65c3f6f6b9bf63b94022ff57ad7e9fc10
desirekaleba <desirekaleba@gmail.com>
2020-04-10 13:17:35 +0300

    handleAction, a function to handle all users actions

---
+++ projects/skill_sharing/client/main.js
@@ -0,0 +1,30 @@
+function handleAction(state, action) {
+    if (action.type == "setUser") {
+        localStorage.setItem("userName", action.user);
+        return Object.assign({}, state, {user: action.user});
+    } else if (action.type == "setTalks") {
+        return Object.assign({}, state, {talks: action.talks});
+    } else if (action.type == "newTalk") {
+        fetchOk(talkURL(action.title), {
+            method: "PUT",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                presenter: state.user,
+                summary: action.summary
+            })
+        }).catch(reportError);
+    } else if (action.type == "deleteTalk") {
+        fetchOk(talkURL(action.talk), {method: "DELETE"})
+            .catch(reportError);
+    } else if (action.type == "newComment") {
+        fetchOk(talkURL(action.talk) + "/comments", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                author: state.user,
+                message: action.message
+            })
+        }).catch(reportError);
+    }
+    return state;
+}
